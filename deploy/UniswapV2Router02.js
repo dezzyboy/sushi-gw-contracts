1c0b18363d85aa5a5c96541cc8d66d066d0401ea
dezzyboy <dezzyboy@tradebit.ca>
2022-04-15 02:59:50 -0400

    init

---
+++ deploy/UniswapV2Router02.js
@@ -0,0 +1,33 @@
+const { WETH } = require("@sushiswap/sdk");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const chainId = await getChainId();
+
+  let wethAddress;
+
+  if (chainId === "31337") {
+    wethAddress = (await deployments.get("WETH9Mock")).address;
+  } else if (chainId in WETH) {
+    wethAddress = WETH[chainId].address;
+  } else if (chainId === "868455272153094") {
+    wethAddress = "0x4B6DFa286F196F61134b03e783d73708687B93D0";
+  } else {
+    throw Error("No WETH!");
+  }
+
+  const factoryAddress = (await deployments.get("UniswapV2Factory")).address;
+
+  await deploy("UniswapV2Router02", {
+    from: deployer,
+    args: [factoryAddress, wethAddress],
+    log: true,
+    deterministicDeployment: false,
+  });
+};
+
+module.exports.tags = ["UniswapV2Router02", "AMM"];
+module.exports.dependencies = ["UniswapV2Factory", "Mocks"];
